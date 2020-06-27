b7b60ef1c49b18a1e6fb8b922ba9045015d3af7e
liaz.kamper <liaz.kamper@appsflyer.com>
2020-06-27 22:42:23 +0300

    Java Simple app: Add reading the afdevkey from an ignored file

--- java/simple_deep_link_pass_data/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/AppsflyerBasicApp.java
+++ java/simple_deep_link_pass_data/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/AppsflyerBasicApp.java
@@ -3,15 +3,15 @@ package com.appsflyer.onelink.appsflyeronelinkbasicapp;
 import android.app.Application;
 
 import com.appsflyer.AppsFlyerLib;
+import com.appsflyer.onelink.appsflyeronelinkbasicapp.AppsFlyerConstants;
 
 public class AppsflyerBasicApp extends Application {
-    public static final String LOG_TAG = "AppsFlyerBasicApp";
+    public static final String LOG_TAG = "AppsFlyerFeedMeApp";
     @Override
     public void onCreate() {
         super.onCreate();
         //noinspection SpellCheckingInspection
-        // TODO - Feel you afDevKey - https://support.appsflyer.com/hc/en-us/articles/207032126-Android-SDK-integration-for-developers#integration-31-retrieving-your-dev-key
-        String afDevKey = "XXXXXXXXXXXXXXX";
+        String afDevKey = AppsFlyerConstants.afDevKey;
         AppsFlyerLib appsflyer = AppsFlyerLib.getInstance();
         appsflyer.setMinTimeBetweenSessions(0);
         appsflyer.init(afDevKey, null, this);
