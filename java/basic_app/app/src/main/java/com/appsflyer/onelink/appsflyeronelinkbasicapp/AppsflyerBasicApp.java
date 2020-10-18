b7c6e2edb2e92f36ffe94f2d31c4387cc6498a84
liaz.kamper <liaz.kamper@appsflyer.com>
2020-10-18 15:55:54 +0300

    To squash - First version with full flow

--- java/basic_app/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/AppsflyerBasicApp.java
+++ java/basic_app/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/AppsflyerBasicApp.java
@@ -4,9 +4,17 @@ import android.app.Application;
 import android.content.Intent;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
 import com.appsflyer.AppsFlyerConversionListener;
 import com.appsflyer.AppsFlyerLib;
+import com.appsflyer.deeplink.DeepLink;
+import com.appsflyer.deeplink.DeepLinkListener;
+import com.appsflyer.deeplink.DeepLinkResult;
 import com.appsflyer.onelink.appsflyeronelinkbasicapp.AppsFlyerConstants;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,70 +31,80 @@ public class AppsflyerBasicApp extends Application {
         AppsFlyerLib appsflyer = AppsFlyerLib.getInstance();
         appsflyer.setMinTimeBetweenSessions(0);
         appsflyer.init(afDevKey, null, this);
-        appsflyer.startTracking(this, afDevKey);
+        appsflyer.start(this, afDevKey);
         appsflyer.setDebugLog(true);
 
-        AppsFlyerLib.getInstance().registerConversionListener(this, new AppsFlyerConversionListener() {
+//        appsflyer.registerConversionListener(this, new AppsFlyerConversionListener() {
+//            @Override
+//            public void onConversionDataSuccess(Map<String, Object> conversionData) {
+//                for (String attrName : conversionData.keySet())
+//                    Log.d(LOG_TAG, "Conversion attribute: " + attrName + " = " + conversionData.get(attrName));
+//                String status = Objects.requireNonNull(conversionData.get("af_status")).toString();
+//                if(status.equals("Non-organic")){
+//                    if( Objects.requireNonNull(conversionData.get("is_first_launch")).toString().equals("true")){
+//                        Log.d(LOG_TAG,"Conversion: First Launch");
+//                    } else {
+//                        Log.d(LOG_TAG,"Conversion: Not First Launch");
+//                    }
+//                } else {
+//                    Log.d(LOG_TAG,"Conversion: This is an organic install.");
+//                }
+//            }
+//
+//            @Override
+//            public void onConversionDataFail(String errorMessage) {
+//                Log.d(LOG_TAG, "error getting conversion data: " + errorMessage);
+//            }
+//
+//            @Override
+//            public void onAppOpenAttribution(Map<String, String> attributionData) {
+//                if (!attributionData.containsKey("is_first_launch"))
+//                    Log.d(LOG_TAG, "onAppOpenAttribution: This is NOT deferred deep linking");
+//                for (String attrName : attributionData.keySet()) {
+//                    String deepLinkAttrStr = attrName + " = " + attributionData.get(attrName);
+//                    Log.d(LOG_TAG, "Deeplink attribute: " + deepLinkAttrStr);
+//                }
+////                Log.d(LOG_TAG, "onAppOpenAttribution: Deep linking into " + attributionData.get("fruit_name"));
+////                goToFruit(attributionData.get("fruit_name"), attributionData);
+//            }
+//
+//            @Override
+//            public void onAttributionFailure(String errorMessage) {
+//                Log.d(LOG_TAG, "error onAttributionFailure : " + errorMessage);
+//            }
+//        });
+
+        appsflyer.subscribeForDeepLink(new DeepLinkListener(){
             @Override
-            public void onConversionDataSuccess(Map<String, Object> conversionData) {
-                for (String attrName : conversionData.keySet())
-                    Log.d(LOG_TAG, "Conversion attribute: " + attrName + " = " + conversionData.get(attrName));
-                //TODO - remove this
-                String status = Objects.requireNonNull(conversionData.get("af_status")).toString();
-                if(status.equals("Non-organic")){
-                    if( Objects.requireNonNull(conversionData.get("is_first_launch")).toString().equals("true")){
-                        Log.d(LOG_TAG,"Conversion: First Launch");
-                        if (conversionData.containsKey("fruit_name")){
-                            Log.d(LOG_TAG,"Conversion: This is deferred deep linking.");
-                            //  TODO SDK in future versions - match the input types
-                            Map<String,String> newMap = new HashMap<>();
-                            for (Map.Entry<String, Object> entry : conversionData.entrySet()) {
-                                newMap.put(entry.getKey(), String.valueOf(entry.getValue()));
-                            }
-                            onAppOpenAttribution(newMap);
-                        }
-                    } else {
-                        Log.d(LOG_TAG,"Conversion: Not First Launch");
-                    }
-                } else {
-                    Log.d(LOG_TAG,"Conversion: This is an organic install.");
+            public void onDeepLinking(@NonNull DeepLinkResult deepLinkResult) {
+                DeepLinkResult.Error dlError = deepLinkResult.getError();
+                DeepLink deepLinkObj = deepLinkResult.getDeepLink();
+                try {
+                    Log.d(LOG_TAG, "The DeepLink data is: " + deepLinkObj.toString());
+                } catch (Exception e) {
+                    Log.d(LOG_TAG, "DeepLink data came back null");
+                    return;
                 }
-            }
-
-            @Override
-            public void onConversionDataFail(String errorMessage) {
-                Log.d(LOG_TAG, "error getting conversion data: " + errorMessage);
-            }
-
-            @Override
-            public void onAppOpenAttribution(Map<String, String> attributionData) {
-                if (!attributionData.containsKey("is_first_launch"))
-                    Log.d(LOG_TAG, "onAppOpenAttribution: This is NOT deferred deep linking");
-                for (String attrName : attributionData.keySet()) {
-                    String deepLinkAttrStr = attrName + " = " + attributionData.get(attrName);
-                    Log.d(LOG_TAG, "Deeplink attribute: " + deepLinkAttrStr);
-                }
-                Log.d(LOG_TAG, "onAppOpenAttribution: Deep linking into " + attributionData.get("fruit_name"));
-                goToFruit(attributionData.get("fruit_name"), attributionData);
-            }
-
-            @Override
-            public void onAttributionFailure(String errorMessage) {
-                Log.d(LOG_TAG, "error onAttributionFailure : " + errorMessage);
+                // TODO - Custom params
+//                    String fruitName = deepLinkObj.getCustomParams().getString("fruit_name");
+                // TODO - af_subX support
+//                String fruitName = deepLinkObj.getAfSub5();
+                String fruitName = "apples";
+                goToFruit(fruitName, deepLinkObj);
             }
         });
     }
 
-    private void goToFruit(String fruitName, Map<String, String> dlData) {
+    private void goToFruit(String fruitName, DeepLink dlData) {
         String fruitClassName = fruitName.concat("Activity");
         try {
             Class fruitClass = Class.forName(this.getPackageName().concat(".").concat(fruitClassName));
             Log.d(LOG_TAG, "Looking for class " + fruitClass);
             Intent intent = new Intent(getApplicationContext(), fruitClass);
             if (dlData != null) {
-                // Map is casted HashMap since it is easier to pass serializable data to an intent
-                HashMap<String, String> copy = new HashMap<String, String>(dlData);
-                intent.putExtra(DL_ATTRS, copy);
+                // TODO - make DeepLink Parcelable
+                String objToStr = new Gson().toJson(dlData);
+                intent.putExtra(DL_ATTRS, objToStr);
             }
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
